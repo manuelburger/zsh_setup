b47c9faaf1499af776d78b818700a8066a2ca3ec
Manuel Burger <burgerm@student.ethz.ch>
2019-10-14 11:31:16 +0200

    Initial setup

---
+++ setup.sh
@@ -0,0 +1,12 @@
+
+# Install Oh-my-ZSH
+echo "Install Oh-my-ZSH..."
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install plugins
+echo "Install plugins..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+echo "Copy .zshrc to home folder..."
+cp ./zshrc ~/
