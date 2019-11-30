59acfcf1a5dbf3d77964c83f08787a958a03c169
Manuel Burger <burgerm@student.ethz.ch>
2019-11-30 20:18:07 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ echo "Install plugins..."
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
 echo "Copy .zshrc to home folder..."
 cp ./.zshrc ~/
